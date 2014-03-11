f5d12879a6ef37de9fb695e32ef98286385bd92b
root <ghares@redhat.com>
2014-03-11 10:02:31 +0000

    Updated

--- oraconf/manifests/init.pp
+++ oraconf/manifests/init.pp
@@ -1,10 +1,7 @@
 
 # Puppet standalone command:   puppet apply --verbose  -e 'include oraconf'
 
-class oraconf ( $oraconf::params::oracleVers    = '11.2',
-                $oraconf::params::oracleBase    = '/u01',
-                $oraconf::params::oracleHome    = '$oracle_base/product/$oracle_vers/db',
-                $oraconf::params::oracleUsrPwd  = 'oracle') {
+class oraconf {
 
   include oraconf::addgroups
   include oraconf::adduser
